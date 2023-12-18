f3b404abdb61e0851dc6eba02802a74f7a26d010
Edvan de Jesus <edvanbolsavalores@gmail.com>
2023-12-18 13:03:26 -0300

    projeto finalizado

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,34 +1,40 @@
 const pianoKeys = document.querySelectorAll(".piano-keys .key");
-let audio = new Audio();
+const volumeSlider = document.querySelector(".volume-slider input");
+const keysCheck = document.querySelector(".keys-check input");
+
+let mapedKeys = [];
+let audio = new Audio("src/tunes/a.wav");
 
 const playTune = (key) => {
-    audio.src = `src/tunes/${key}.wav`;
-    audio.play();
+  audio.src = `src/tunes/${key}.wav`;
+  audio.play();
+
+  const clickedKey = document.querySelector(`[data-key="${key}"]`);
+  clickedKey.classList.add("active");
+  setTimeout(() => {
+    clickedKey.classList.remove("active");
+  }, 150);
 };
 
 pianoKeys.forEach((key) => {
-    key.addEventListener("click", () => {
-        const keyData = key.dataset.key;
-        console.log(keyData);
-        playTune(keyData);
-
-        key.classList.add("active");
-        setTimeout(() => {
-            key.classList.remove("active");
-        }, 150);
-    });
+  key.addEventListener("click", () => playTune(key.dataset.key));
+  mapedKeys.push(key.dataset.key);
 });
 
 document.addEventListener("keydown", (e) => {
-    const key = e.key;
-    console.log(key);
-    playTune(key);
-
-    const clickedKey = document.querySelector(`[data-key="${key}"]`);
-    if (clickedKey) {
-        clickedKey.classList.add("active");
-        setTimeout(() => {
-            clickedKey.classList.remove("active");
-        }, 150);
-    }
+  if (mapedKeys.includes(e.key)) {
+    playTune(e.key);
+  }
 });
+
+const handleVolume = (e) => {
+  audio.volume = e.target.value;
+};
+
+const showHideKeys = () => {
+  pianoKeys.forEach((key) => key.classList.toggle("hide"));
+};
+
+volumeSlider.addEventListener("input", handleVolume);
+
+keysCheck.addEventListener("click", showHideKeys);
